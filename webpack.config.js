9c74e7023f436209894c7e929616b2ed3b53ea7c
Rigo De La Torre <rigoddd@gmail.com>
2018-09-27 00:18:46 -0700

    turned js webpack off

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ const VENDOR_LIBS = [
 
 module.exports = {
   entry: {
-    firstComp: './assets/js/firstComp/firstComp.js',
-    vendor: VENDOR_LIBS
+    // firstComp: './assets/js/firstComp/firstComp.js',
+    // vendor: VENDOR_LIBS
   },
   output: { filename: '[name].js',
             path: path.resolve(__dirname, 'public/js/components') },
